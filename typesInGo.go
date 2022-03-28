dcf1e91d69c7adaac91630249d7f3d632d8140ff
Alexander Wong <alexandthemachine@gmail.com>
2022-03-28 16:58:42 +0800

    Made a struct to try this

--- typesInGo.go
+++ typesInGo.go
@@ -1,11 +1,46 @@
 package main
 
+import "fmt"
+
+type person struct {
+	firstName        string
+	lastName         string
+	age              int
+	weight           float32
+	height           float32
+	remainingCredits float64
+	accountName      string
+	accountPassword  string
+	subscribedUser   bool
+}
+
 //Insert variables declarations here based on activity
 
-func tellMeTypes() {
+func assignTypesForLuke() *person {
+	p := person{firstName: "Luke"}
+	p.lastName = "Skywalker"
+	p.age = 20
+	p.weight = 73.0
+	p.height = 1.72
+	p.remainingCredits = 123.55
+	p.accountName = "admin"
+	p.accountPassword = "password"
+	p.subscribedUser = true
+
+	return &p
+}
+
+func tellMeTypes(p *person) {
 	//insert code here to print out types of variables
+	text := "The following is the account information."
+
+	fmt.Printf("%T %T %T %T %T %T %T %T %T %T", text, p.firstName,
+		p.lastName, p.age, p.weight, p.height, p.remainingCredits,
+		p.accountName, p.accountPassword, p.subscribedUser)
+
 }
 
 func main() {
-	tellMeTypes()
+	luke := assignTypesForLuke()
+	tellMeTypes(luke)
 }
